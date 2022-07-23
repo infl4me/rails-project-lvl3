4cfa331d38b927959a8013d305bf82b7471da746
udf <hellpl4y@gmail.com>
2022-07-23 11:12:45 +0500

    update bulletin policy

--- app/policies/bulletin_policy.rb
+++ app/policies/bulletin_policy.rb
@@ -10,14 +10,14 @@ class BulletinPolicy < ApplicationPolicy
   end
 
   def create?
-    user_exists?
+    user_exists? || user_admin?
   end
 
   def update?
-    user_owns_record?
+    user_owns_record? || user_admin?
   end
 
   def destroy?
-    user_owns_record?
+    user_owns_record? || user_admin?
   end
 end
